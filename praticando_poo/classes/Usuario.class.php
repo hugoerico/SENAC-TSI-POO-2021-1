335562ac28f02fa1c99666cb44fc993ae5ac3393
SAsuke019 <hugo.019@hotmail.com>
2021-04-20 23:06:57 -0300

    codigo final

--- praticando_poo/classes/Usuario.class.php
+++ praticando_poo/classes/Usuario.class.php
@@ -35,11 +35,35 @@ Class Usuario extends TipoPessoa implements iUsuario {
 		}
 	}
 
+	public function delete():bool
+	{
+
+		if ( $this->id ) {
+
+			$stmt = $this->prepare('DELETE FROM usuarios WHERE id = :id');
+
+			if ($stmt->execute([':id'=>$this->id])) {
+
+				return true;
+
+			} else {
+
+				return false;
+			}
+
+		} else {
+
+			return false;
+		}
+	}
+
 	public function update()
 	{
 		$stmt = $this->prepare('UPDATE usuarios SET cpf = :cpf, nome = :nome WHERE id = :id');
 
-		if( $stmt->execute([':cpf'	=> $this->cpf, ':nome' => $this->nome,':id'	=> $this->id])){
+		if( $stmt->execute([	':cpf'	=> $this->cpf, 
+								':nome' => $this->nome,
+								':id'	=> $this->id])){
 
 			return true;
 		}
@@ -62,5 +86,14 @@ Class Usuario extends TipoPessoa implements iUsuario {
 	public function getDados( int $id_usuario):array
 	{
 
+	}
+
+	public function getAll():array
+	{
+		$stmt = $this->prepare('SELECT * FROM usuarios');
+
+		$stmt->execute();
+
+		return $stmt->fetchAll();
 	}
 }
